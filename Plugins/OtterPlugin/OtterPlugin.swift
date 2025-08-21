b7543f852c69d431777ca1a0f4ac9df896837ba6
wes <wickwirew@gmail.com>
2025-08-20 23:35:21 -0400

    Support xcode projs

--- Plugins/OtterPlugin/OtterPlugin.swift
+++ Plugins/OtterPlugin/OtterPlugin.swift
@@ -6,6 +6,7 @@
 //
 
 import PackagePlugin
+import Foundation
 
 @main
 struct OtterPlugin: BuildToolPlugin {
@@ -13,32 +14,59 @@ struct OtterPlugin: BuildToolPlugin {
         context: PluginContext,
         target: Target
     ) throws -> [Command] {
-        guard target is SourceModuleTarget else { return [] }
-        
-        let sourceRoot = context.package.directoryURL.absoluteString
-        
-        let queries = context.pluginWorkDirectoryURL
-            .appending(component: "Queries.swift")
-        
-        let inputFiles = target.sourceModule?.sourceFiles
-            .filter { $0.url.pathExtension == "sql" }
-            .map(\.url)
-        
         return [
-            .buildCommand(
-                displayName: "Running otter generate",
-                executable: try context.tool(named: "OtterCLI").url,
-                arguments: [
-                    "generate",
-                    "--path",
-                    sourceRoot,
-                    "--override-output",
-                    queries.absoluteString,
-                    "--skip-directory-create"
-                ],
-                inputFiles: inputFiles ?? [],
-                outputFiles: [queries]
+            createBuildCommand(
+                projectRoot: context.package.directoryURL,
+                cliToolURL: try context.tool(named: "OtterCLI").url,
+                sourceFiles: target.sourceModule?.sourceFiles,
+                workDirectory: context.pluginWorkDirectoryURL
+            )
+        ]
+    }
+    
+    private func createBuildCommand(
+        projectRoot: URL,
+        cliToolURL: URL,
+        sourceFiles: FileList?,
+        workDirectory: URL
+    ) -> Command {
+        let queries = workDirectory.appending(component: "Queries.swift")
+        
+        return .buildCommand(
+            displayName: "Running otter generate",
+            executable: cliToolURL,
+            arguments: [
+                "generate",
+                "--path",
+                projectRoot.absoluteString,
+                "--override-output",
+                queries.absoluteString,
+                "--skip-directory-create"
+            ],
+            inputFiles: sourceFiles?
+                .filter { $0.url.pathExtension == "sql" }
+                .map(\.url) ?? [],
+            outputFiles: [queries]
+        )
+    }
+}
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension OtterPlugin: XcodeBuildToolPlugin {
+    func createBuildCommands(
+        context: XcodePluginContext,
+        target: XcodeTarget
+    ) throws -> [Command] {
+        return [
+            createBuildCommand(
+                projectRoot: context.xcodeProject.directoryURL,
+                cliToolURL: try context.tool(named: "OtterCLI").url,
+                sourceFiles: target.inputFiles,
+                workDirectory: context.pluginWorkDirectoryURL
             )
         ]
     }
 }
+#endif
